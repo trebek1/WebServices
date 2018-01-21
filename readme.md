aa1b2527aa4a7ce716184c0d2d821921190c7c3a
Alex Trzeciak <trebek08@gmail.com>
2018-01-21 13:28:29 -0800

    setup basic spring example from start spring io

--- readme.md
+++ readme.md
@@ -1,2 +1,7 @@
 Repo for WebServices Course 
 
+Created spring-start folder strucuture using start.spring.io
+This initial project is to get the basics of tight coupling and dependency injection 
+
+
+
---
+++ spring-start/src/main/java/com/springcourse/spring/basics/springstart/SpringStartApplication.java
@@ -0,0 +1,12 @@
+package com.springcourse.spring.basics.springstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStartApplication.class, args);
+	}
+}
