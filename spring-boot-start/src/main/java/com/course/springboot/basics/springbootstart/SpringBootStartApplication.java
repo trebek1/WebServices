13eb4de1280e4b9fc22cad3850b04df987a58f67
Alex Trzeciak <trebek08@gmail.com>
2018-01-22 18:09:49 -0800

    u[master] setup spring boot dev tools and get hal service working

--- spring-boot-start/src/main/java/com/course/springboot/basics/springbootstart/SpringBootStartApplication.java
+++ spring-boot-start/src/main/java/com/course/springboot/basics/springbootstart/SpringBootStartApplication.java
@@ -2,11 +2,17 @@ package com.course.springboot.basics.springbootstart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class SpringBootStartApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootStartApplication.class, args);
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(SpringBootStartApplication.class, args);
+		
+//		for(String string : applicationContext.getBeanDefinitionNames()) {
+//			System.out.println(string);
+//		}
+		
 	}
 }
